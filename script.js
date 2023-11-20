cafd67a6040d8f8845ac2c3524bf37bd280f51ba
eshpuff <eshileyygarimm@gmail.com>
2023-11-19 22:34:36 -0300

    divs escondidinhas

--- script.js
+++ script.js
@@ -9,3 +9,28 @@ function mascaraCPF() {
 
     cpf.value = valor.slice(0, 14);
 }
+
+function mostrarPagamento() {
+    var DIVpagamento = document.getElementById("pagamento");
+    DIVpagamento.style.display = "block";
+}
+
+function produtoSelecionado(IDproduto) {
+    var DIVpagamento = document.getElementById("pagamento");
+    DIVpagamento.style.display = "none";
+
+    if (IDproduto === "omelete" || IDproduto === "salmao" || IDproduto === "bolo") {
+        mostrarPagamento();
+    } else {
+        DIVpagamento.style.display = "none";
+    }
+}
+
+function mostrarDivPagamento(divID) {
+    var DIVspagamento = document.getElementsByClassName("pagamentoDiv");
+    for (var i = 0; i < DIVspagamento.length; i++) {
+        DIVspagamento[i].style.display = "none";
+    }
+    var divPagamento = document.getElementById(divID);
+    divPagamento.style.display = "block";
+}
